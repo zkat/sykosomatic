3f519b921d1a3adb52cd4ae79dd1755322c90b98
Kat Marchán <kzm@sykosomatic.org>
2011-03-12 17:41:51 -0500

    Ajaxy storytelling.

--- belletrist.lisp
+++ belletrist.lisp
@@ -1,26 +1,48 @@
 (cl:in-package #:belletrist)
 
 (defvar *server* nil)
+(defvar *ajax-processor* (make-instance 'ajax-processor :server-uri "/ajax"))
+(defparameter *current-story* (list "Once upon a time..."))
 
 (defun begin-shared-hallucination ()
-  (start (setf *server* (make-instance 'acceptor :port 8888))))
+  (when *server* (end-shared-hallucination) (print "Restarting..."))
+  (start (setf *server* (make-instance 'acceptor :port 8888)))
+  (setf *dispatch-table* (list 'dispatch-easy-handlers
+                               (create-ajax-dispatcher *ajax-processor*)))
+  t)
 
 (defun end-shared-hallucination ()
   (when *server* (stop *server*) (setf *server* nil)))
 
-(define-easy-handler (home :uri "/") (what-user-said start-over)
+(define-easy-handler (home :uri "/") ()
   (unless *session*
     (start-session))
-  (when start-over (setf (session-value 'what-user-said) nil))
-  (when what-user-said (push what-user-said (session-value 'what-user-said)))
   (with-yaclml-output-to-string
     (<:html
+     (<:head
+      (<:title "Hello!")
+      (<:ai (generate-prologue *ajax-processor*))
+      (<:script :type "text/javascript" (<:ai "
+function callback(response) {
+  var story = response.firstChild.firstChild.nodeValue;
+  document.getElementById('chat-box').innerHTML = story;
+};
+
+function addMsg() {
+  ajax_add_message(document.getElementById('what-user-said').value, callback);
+};
+")))
      (<:body
-      (<:p (<:ah "You are in a dark, fluffy dungeon. " (or (reverse (session-value 'what-user-said))
-                                                           "That is all.")))
-      (<:form :name "user-story" :action "/" :method "get"
+      (<:div :id "chat-box")
+      (<:form :name "user-story" :action "javascript:addMsg()"
        (<:ah "What do?")
-       (<:input :type "text" :name "what-user-said"))
-      (<:br)
-      (<:form :name "start-over" :action "/" :method "get"
-       (<:input :type "submit" :name "start-over" :value "Start over"))))))
+       (<:input :type "text" :id "what-user-said")
+       (<:input :type "submit" :value "Send"))))))
+
+(defun-ajax add-message (msg) (*ajax-processor*)
+  (push (format nil "User sez: ~A" msg) *current-story*)
+  (with-yaclml-output-to-string
+    (<:ah
+     (with-yaclml-output-to-string
+       (mapc (lambda (line) (<:p (<:ai line)))
+             (reverse *current-story*))))))
