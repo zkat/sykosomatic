1ea19d7bc7792db832f5c609bed3aa394503cfc7
Josh Marchán <sykopomp@sykosomatic.org>
2011-03-12 08:57:22 -0500

    Accepting user input and displaying it.

--- belletrist.lisp
+++ belletrist.lisp
@@ -8,8 +8,18 @@
 (defun end-shared-hallucination ()
   (when *server* (stop *server*) (setf *server* nil)))
 
-(define-easy-handler (home :uri "/") ()
+(defparameter *what-user-has-said* nil)
+
+(define-easy-handler (home :uri "/") (what-user-said start-over)
+  (when start-over (setf *what-user-has-said* nil))
+  (when what-user-said (push what-user-said *what-user-has-said*))
   (with-yaclml-output-to-string
     (<:html
      (<:body
-      (<:p (<:ah "You are in a dark, stinky dungeon. That is all."))))))
+      (<:p (<:ah "You are in a dark, fluffy dungeon. " (or (reverse *what-user-has-said*) "That is all.")))
+      (<:form :name "user-story" :action "/" :method "get"
+       (<:ah "What do?")
+       (<:input :type "text" :name "what-user-said"))
+      (<:br)
+      (<:form :name "start-over" :action "/" :method "get"
+       (<:input :type "submit" :name "start-over" :value "Start over"))))))
