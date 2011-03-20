10b8792cf216ef7b199656059dcfbb57cfaced67
Josh Marchán <sykopomp@sykosomatic.org>
2011-03-20 16:29:47 -0400

    Added display names.

--- src/account.lisp
+++ src/account.lisp
@@ -79,12 +79,40 @@
                                        if (doc.type == 'account') {
                                          emit([doc.account_name.toLowerCase(),doc.password],doc);
                                        }
+                                     }")
+                                    "by_display_name"
+                                    (mkdoc "map"
+                                     "function (doc) {
+                                       if (doc.type == 'account') {
+                                         emit(doc.display_name.toLowerCase(),doc);
+                                       }
                                      }")))))
+
+(defun find-account-by-display-name (display-name)
+  (when-let ((results (doc-val (query-view *db* "account" "by_display_name"
+                                           :key (string-downcase display-name))
+                               "rows")))
+    (doc-val (car results)
+             "value")))
+
+(defun find-account (account-name)
+  (when-let ((results (doc-val (query-view *db* "account" "by_account_name"
+                                           :key (string-downcase account-name))
+                               "rows")))
+    (doc-val (car results)
+             "value")))
+
+(defun validate-credentials (account-name password &aux (hashed-pass (hash-password password)))
+  (when-let ((results (doc-val (query-view *db* "account" "by_account_name_password"
+                                           :key (list (string-downcase account-name) hashed-pass))
+                               "rows")))
+    (doc-val (car results)
+             "value")))
 
 (defparameter *email-regex* (create-scanner "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$"
                                             :case-insensitive-mode t))
 
-(defparameter *display-name-regex* (create-scanner "^[A-Z_\\-.']$"
+(defparameter *display-name-regex* (create-scanner "^[A-Z0-9._.-]+$"
                                                    :case-insensitive-mode t))
 
 (defun valid-email-p (email)
@@ -121,37 +149,27 @@
 (defun assert-required (fieldname  x)
   (assert-validation (not (emptyp x)) (format nil "~A is required." fieldname)))
 
-(defun validate-new-account (account-name password confirmation)
+(defun validate-new-account (account-name display-name password confirmation)
   (with-validation
     (assert-required "Account name" account-name)
+    (assert-required "Display Name" display-name)
     (assert-required "Password" password)
     (assert-required "Confirmation" confirmation)
     (assert-validation (valid-email-p account-name) "Invalid email.")
     (assert-validation (not (find-account account-name)) "Account already exists.")
     (assert-validation (valid-password-p password) "Invalid password.")
+    (assert-validation (valid-display-name-p display-name) "Invalid display name.")
+    (assert-validation (not (find-account-by-display-name display-name)) "Display name already in use.")
     (assert-validation (string= password confirmation) "Password confirmation does not match.")))
 
-(defun create-account (account-name password confirmation
+(defun create-account (account-name display-name password confirmation
                        &aux (hashed-pass (hash-password password)))
   (multiple-value-bind (validp errors)
-      (validate-new-account account-name password confirmation)
+      (validate-new-account account-name display-name password confirmation)
     (if validp
         (ensure-doc (get-uuid)
                     (mkdoc "type" "account"
                            "account_name" account-name
+                           "display_name" display-name
                            "password" hashed-pass))
         (values nil errors))))
-
-(defun find-account (account-name)
-  (when-let ((results (doc-val (query-view *db* "account" "by_account_name"
-                                           :key (string-downcase account-name))
-                               "rows")))
-    (doc-val (car results)
-             "value")))
-
-(defun validate-credentials (account-name password &aux (hashed-pass (hash-password password)))
-  (when-let ((results (doc-val (query-view *db* "account" "by_account_name_password"
-                                           :key (list (string-downcase account-name) hashed-pass))
-                               "rows")))
-    (doc-val (car results)
-             "value")))
