3455ff474aaadd14f8deaf25b0dbf37fd97bf8bc
Josh Marchán <sykopomp@sykosomatic.org>
2011-03-27 21:07:00 -0400

    Scene-saving baby step.

--- src/scene.lisp
+++ src/scene.lisp
@@ -1,7 +1,17 @@
 (cl:defpackage #:sykosomatic.scene
-  (:use :cl :chillax.core :sykosomatic.db))
+  (:use :cl :chillax.core :sykosomatic.db)
+  (:export :create-scene))
 (cl:in-package #:sykosomatic.scene)
 
-(defun create-scene ()
+(defun ensure-scene-design-doc ()
+  (ensure-doc "_design/scene"
+              (mkdoc "language" "common-lisp"
+                     "views" (mkdoc "by_account_name"
+                                    (mkdoc "map"
+                                           (mapfun doc "scene"
+                                             (emit (string-downcase (hashget doc "account_name"))
+                                                   doc)))))))
+(defun create-scene (account-name)
   (ensure-doc (get-uuid)
-              (mkdoc "type" "scene")))
+              (mkdoc "type" "scene"
+                     "account_name" account-name)))
