d1425ddbf0ee7e13d36a7629d1812fb588a9f3c9
Kat Marchán <kzm@sykosomatic.org>
2011-04-17 16:29:47 -0400

    DBG utility.

--- src/utils.lisp
+++ src/utils.lisp
@@ -1,7 +1,11 @@
 (cl:defpackage #:sykosomatic.utils
   (:use :cl)
-  (:export :logit))
+  (:export :logit :dbg))
 (cl:in-package :sykosomatic.utils)
 
 (defun logit (format-string &rest format-args)
   (format t "~&~A~%" (apply #'format nil format-string format-args)))
+
+(defun dbg (comment obj)
+  (format t "DBG XXX: ~A (~S)" comment obj)
+  obj)
