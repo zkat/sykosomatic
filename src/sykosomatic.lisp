32b866a64d02a1d7bda984850a0abc5c2a70fc21
Josh Marchán <sykopomp@sykosomatic.org>
2011-04-16 20:40:11 -0400

    Revert "Blowing away render-user-action."
    This reverts commit 0596ad2041a06f36eb3980779278b54723212cac.
    
    Conflicts:
    
    	src/sykosomatic.lisp

--- src/sykosomatic.lisp
+++ src/sykosomatic.lisp
@@ -15,3 +15,22 @@
   (session-value 'websocket-clients session))
 (defun (setf session-websocket-clients) (new-value session)
   (setf (session-value 'websocket-clients session) new-value))
+
+(defun render-user-action (user-action)
+  (let ((action (user-action-action user-action))
+        (dialogue (user-action-dialogue user-action)))
+    (<:div :class "user-entry"
+      (if (and (not (emptyp action)) (emptyp dialogue))
+          (<:p :class "action"
+               (<:ah (user-action-user user-action) " " action))
+          (progn
+            (<:p :class "character"
+                 (<:ah (user-action-user user-action)))
+            (unless (emptyp action)
+              (<:p :class "parenthetical"
+                   (<:ah "(" action ")")))
+            (<:p :class "dialogue"
+                 (<:ah
+                  (if (emptyp dialogue)
+                      "..."
+                      dialogue))))))))
