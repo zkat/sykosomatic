0282e7f347dee689d7a71a93e94eb1c5d2ec9766
Josh Marchán <sykopomp@sykosomatic.org>
2011-07-30 17:40:05 -0400

    Simplify, simplify, simplify.

--- src/templates.lisp
+++ src/templates.lisp
@@ -251,58 +251,55 @@
                                                       (list first-name nickname last-name pronoun))))
   (yaclml:with-yaclml-output-to-string
     (<:p (<:ah "You are creating a new character.")
-         (<:ah (when fully-named
-                 (format nil " ~:(~A~) name is ~:(~A~) ~:(~A~), also known as \"~A\"."
-                         (possessive pronoun) first-name last-name nickname))))
-    (when fully-named
-      (<:p (<:ah (format nil "~:(~A~) ~A originally from ~A."
-                         pronoun (if pluralp "are" "is")
-                         (cond
-                           ((string= origin "local")
-                            "the Twin Cities")
-                           ((string= origin "state")
-                            "Minnesota")
-                           ((string= origin "midwest")
-                            "somewhere in the Midwest")
-                           ((string= origin "east-coast")
-                            "the East Coast")
-                           ((string= origin "south")
-                            "the South")
-                           ((string= origin "west-coast")
-                            "the West Coast")
-                           (t "outside the continental US"))))))
-    (when fully-named
-     (<:p (<:ah (format nil "Brought up ~A ~A parent~P, ~:(~A~) was one of ~A
+         (<:ah (if fully-named
+                   (format nil " ~:(~A~) name is ~:(~A~) ~:(~A~), also known as \"~A\"."
+                           (possessive pronoun) first-name last-name nickname)
+                   (format nil " ~:(~A~) ~A been fully named yet."
+                           pronoun (if pluralp "haven't" "hasn't")))))
+    (<:p (<:ah (format nil "~:(~A~) ~A originally from ~A."
+                       pronoun (if pluralp "are" "is")
+                       (cond
+                         ((string= origin "local")
+                          "the Twin Cities")
+                         ((string= origin "state")
+                          "Minnesota")
+                         ((string= origin "midwest")
+                          "somewhere in the Midwest")
+                         ((string= origin "east-coast")
+                          "the East Coast")
+                         ((string= origin "south")
+                          "the South")
+                         ((string= origin "west-coast")
+                          "the West Coast")
+                         (t "outside the continental US")))))
+    (<:p (<:ah (format nil "Brought up ~A ~A parent~P, ~:(~A~) was one of ~A
     children in a ~A family."
-                        (string-case (parents)
-                          ("none" "without")
-                          (t "by"))
-                        (string-case (parents)
-                          ("more" "several")
-                          ("none" "any")
-                          (t parents))
-                        (string-case (parents)
-                          ("one" 1)
-                          (t 3))
-                        nickname
-                        (string-case (siblings)
-                          ("more" "several")
-                          ("one" "two")
-                          ("two" "three")
-                          ("three" "four"))
-                        class))))
-    (when fully-named
-      (<:p (<:ah (format nil "Now ~:(~A~) is ~A years old. ~:(~A~) ~A
+                       (string-case (parents)
+                         ("none" "without")
+                         (t "by"))
+                       (string-case (parents)
+                         ("more" "several")
+                         ("none" "any")
+                         (t parents))
+                       (string-case (parents)
+                         ("one" 1)
+                         (t 3))
+                       nickname
+                       (string-case (siblings)
+                         ("more" "several")
+                         ("one" "two")
+                         ("two" "three")
+                         ("three" "four"))
+                       class)))
+    (<:p (<:ah (format nil "Now ~:(~A~) is ~A years old. ~:(~A~) ~A
       ~A. Also, ~(~A~) ~A ~A. Having built a career in ~A for the
       past ~A, ~A has ~A."
-                         nickname "AGE" pronoun (if pluralp "have" "has") "FRIENDSHIP-STATUS"
-                         pronoun (if pluralp "are" "is") "DATING-STATUS"
-                         "CAREER1" "CAREER1-LENGTH" pronoun "OTHER-CAREERS"))))
-    (when fully-named
-      (<:p (<:ah (format nil "~:(~A~) is currently staying in ~A."
-                         nickname "HERE-AND-NOW"))))
-    (when fully-named
-      (<:p (<:ah (format nil "~:(~A~)'s journey is about to begin." nickname))))))
+                       nickname "AGE" pronoun (if pluralp "have" "has") "FRIENDSHIP-STATUS"
+                       pronoun (if pluralp "are" "is") "DATING-STATUS"
+                       "CAREER1" "CAREER1-LENGTH" pronoun "OTHER-CAREERS")))
+    (<:p (<:ah (format nil "~:(~A~) is currently staying in ~A."
+                       nickname "HERE-AND-NOW")))
+    (<:p (<:ah (format nil "~:(~A~)'s journey is about to begin." nickname)))))
 
 ;;; /newchar
 (defun newchar-js ()
