a977abaf9afa2097c89cefbb27beea83ea1f0fac
Kat Marchán <kzm@sykosomatic.org>
2011-07-31 21:55:40 -0400

    Some cleanup.

--- src/templates.lisp
+++ src/templates.lisp
@@ -239,69 +239,6 @@
            (text-input-field "confirmation" "Confirm password" :type "password"))
           (<:submit :value "Submit")))
 
-;;; /newchar-preview
-(defun possessive (pronoun)
-  (string-case (pronoun)
-    ("he" "his")
-    ("she" "her")
-    ("they" "their")))
-
-(defun newchar-preview-div (&key first-name nickname last-name pronoun pluralp origin
-                            parents siblings class
-                            &aux (fully-named (notany #'emptyp
-                                                      (list first-name nickname last-name pronoun))))
-  (yaclml:with-yaclml-output-to-string
-    (<:p (<:ah "You are creating a new character.")
-         (<:ah (if fully-named
-                   (format nil " ~:(~A~) name is ~:(~A~) ~:(~A~), also known as \"~A\"."
-                           (possessive pronoun) first-name last-name nickname)
-                   (format nil " ~:(~A~) ~A been fully named yet."
-                           pronoun (if pluralp "haven't" "hasn't")))))
-    (<:p (<:ah (format nil "~:(~A~) ~A originally from ~A."
-                       pronoun (if pluralp "are" "is")
-                       (cond
-                         ((string= origin "local")
-                          "the Twin Cities")
-                         ((string= origin "state")
-                          "Minnesota")
-                         ((string= origin "midwest")
-                          "somewhere in the Midwest")
-                         ((string= origin "east-coast")
-                          "the East Coast")
-                         ((string= origin "south")
-                          "the South")
-                         ((string= origin "west-coast")
-                          "the West Coast")
-                         (t "outside the continental US")))))
-    (<:p (<:ah (format nil "Brought up ~A ~A parent~P, ~:(~A~) was one of ~A
-    children in a ~A family."
-                       (string-case (parents)
-                         ("none" "without")
-                         (t "by"))
-                       (string-case (parents)
-                         ("more" "several")
-                         ("none" "any")
-                         (t parents))
-                       (string-case (parents)
-                         ("one" 1)
-                         (t 3))
-                       nickname
-                       (string-case (siblings)
-                         ("more" "several")
-                         ("one" "two")
-                         ("two" "three")
-                         ("three" "four"))
-                       class)))
-    (<:p (<:ah (format nil "Now ~:(~A~) is ~A years old. ~:(~A~) ~A
-      ~A. Also, ~(~A~) ~A ~A. Having built a career in ~A for the
-      past ~A, ~A has ~A."
-                       nickname "AGE" pronoun (if pluralp "have" "has") "FRIENDSHIP-STATUS"
-                       pronoun (if pluralp "are" "is") "DATING-STATUS"
-                       "CAREER1" "CAREER1-LENGTH" pronoun "OTHER-CAREERS")))
-    (<:p (<:ah (format nil "~:(~A~) is currently staying in ~A."
-                       nickname "HERE-AND-NOW")))
-    (<:p (<:ah (format nil "~:(~A~)'s journey is about to begin." nickname)))))
-
 ;;; /newchar
 (defun newchar-js ()
   (<:script :type "text/javascript" :src "res/newchar.js"))
