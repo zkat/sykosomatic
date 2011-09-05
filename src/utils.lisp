c44b8f3b67d7a5e87c18039468eaae18aa56a1b3
Kat Marchán <kzm@sykosomatic.org>
2011-09-05 17:32:42 -0400

    Added for share/update support to s-sql

--- src/utils.lisp
+++ src/utils.lisp
@@ -31,3 +31,20 @@
 (defun random-string (length &optional (dictionary "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"))
   (map-into (make-array length :element-type 'character)
             (curry #'random-elt dictionary)))
+
+(cl:in-package :s-sql)
+
+(defun for-update/share (share-or-update form &rest args)
+  (let* ((of-position (position :of args))
+         (no-wait-position (position :nowait args))
+         (of-tables (when of-position (subseq args (1+ of-position) no-wait-position))))
+    `("(" ,@(sql-expand form) ,(format nil " FOR ~:@(~A~)" share-or-update)
+          ,@(when of-tables (list (format nil " OF ~{~A~^, ~}" (mapcar #'sql-expand of-tables))))
+          ,@(when no-wait-position (list " NOWAIT"))
+          ")")))
+
+(def-sql-op :for-update (form &rest args)
+  (apply #'for-update/share "UPDATE" form args))
+
+(def-sql-op :for-share (form &rest args)
+  (apply #'for-update/share "SHARE" form args))
