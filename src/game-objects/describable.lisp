79182e55c79cf9d6ac05dc57f0de36d3f9db8e10
Josh Marchán <sykopomp@sykosomatic.org>
2011-09-05 15:44:30 -0400

    Game object description example.

---
+++ src/game-objects/describable.lisp
@@ -0,0 +1,24 @@
+(cl:defpackage #:sykosomatic.game-objects.describable
+  (:use :cl :alexandria :sykosomatic.entity :sykosomatic.db :sykosomatic.utils :postmodern))
+(cl:in-package #:sykosomatic.game-objects.describable)
+
+(optimizations)
+
+(defun short-name (entity)
+  (text-modifier-value entity "desc:short-name"))
+
+(defun full-name (entity)
+  (with-db ()
+    (when-let (short-name (short-name entity))
+      (let ((article (text-modifier-value entity "desc:article"))
+            (adjectives (query (:select 'text-value :from 'modifier
+                                        :where (:and (:= 'entity-id entity)
+                                                     (:= 'type "desc:adjective")))
+                               :column)))
+        (format nil "~@[~A ~]~{~A ~^~}~A" article adjectives short-name)))))
+
+(defun short-description (entity)
+  (text-modifier-value entity "desc:short-description"))
+
+(defun long-description (entity)
+  (text-modifier-value entity "desc:long-description"))
