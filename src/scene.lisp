8eb664748aa6ba55256215e532df6e3245373b9f
Josh Marchán <sykopomp@sykosomatic.org>
2011-09-04 20:15:07 -0400

    New pomo-based sykosomatic.scene

--- src/scene.lisp
+++ src/scene.lisp
@@ -1,100 +1,87 @@
 (cl:defpackage #:sykosomatic.scene
-  (:use :cl :alexandria :chillax.core :sykosomatic.db)
-  (:export :create-scene :add-action :add-dialogue :ensure-scene-design-doc
-           :find-scenes-by-account-name :find-scene-with-entries :find-scene-entries
+  (:use :cl :alexandria :postmodern :sykosomatic.db)
+  (:export :create-scene :add-action :add-dialogue
+           :find-scenes-by-account-email :find-scene-with-entries :find-scene-entries
            :scene-id :scene-upvote :scene-rating))
 (cl:in-package #:sykosomatic.scene)
 
-(defun ensure-scene-design-doc ()
-  (ensure-doc "_design/scene"
-              (mkdoc "language" "common-lisp"
-                     "views" (mkdoc "by_account_name"
-                                    (mkdoc "map"
-                                           (mapfun doc "scene"
-                                             (emit (string-downcase (hashget doc "account_name"))
-                                                   doc)))
-                                    "full_by_id"
-                                    (mkdoc "map"
-                                           (couchfun (doc &aux (type (hashget doc "type")))
-                                             (cond ((string= type "scene")
-                                                    (emit (list (hashget doc "_id") 0) doc))
-                                                   ((string= type "scene-entry")
-                                                    (emit (list (hashget doc "scene_id")
-                                                                1
-                                                                (hashget doc "timestamp"))
-                                                          doc))
-                                                   (t nil))))
-                                    "entries_by_scene_id"
-                                    (mkdoc "map"
-                                           (mapfun doc "scene-entry"
-                                             (emit (hashget doc "scene_id")
-                                                   doc)))
-                                    "rating_by_scene_id"
-                                    (mkdoc "map"
-                                           (mapfun doc "scene-rating"
-                                             (emit (hashget doc "scene_id") 1))
-                                           "reduce"
-                                           (couchfun (keys vals &optional rrp)
-                                             (declare (ignore keys rrp))
-                                             (reduce #'+ vals :initial-value 0)))
-                                    "rating_by_account_name"
-                                    (mkdoc "map"
-                                           (mapfun doc "scene-rating"
-                                             (emit (list (hashget doc "scene_id")
-                                                         (hashget doc "account_name")) doc)))))))
+(defdao scene ()
+  ((id :col-type serial :reader id)
+   (account-id :col-type bigint :initarg :account-id))
+  (:keys id))
+(defun create-scene (account-email)
+  (make-dao 'scene :account-id (query (:select 'id :from 'account :where (:= 'email account-email))
+                                      :single)))
 
-(defun create-scene (account-name)
-  (let ((uuid (get-uuid)))
-    (ensure-doc uuid
-                (mkdoc "type" "scene"
-                       "account_name" account-name))
-    uuid))
+(defdao scene-action ()
+  ((id :col-type serial :reader id)
+   (scene-id :col-type bigint :initarg :scene-id)
+   (actor-id :col-type bigint :initarg :actor-id)
+   (action :col-type text :initarg :action)
+   (timestamp :col-type timestamp :col-default (:now) :initarg :timestamp))
+  (:keys id))
+(defun add-action (scene-id actor-id action-txt)
+  (make-dao 'scene-action
+            :scene-id scene-id
+            :actor-id actor-id
+            :action action-txt))
 
-(defun add-action (scene-id actor action-txt &key (timestamp (get-universal-time)))
-  (ensure-doc (get-uuid)
-              (mkdoc "type" "scene-entry"
-                     "entry_type" "action"
-                     "scene_id" scene-id
-                     "actor" actor
-                     "action" action-txt
-                     "timestamp" timestamp)))
+(defdao scene-dialogue ()
+  ((id :col-type serial :reader id)
+   (scene-id :col-type bigint :initarg :scene-id)
+   (actor-id :col-type bigint :initarg :actor-id)
+   (dialogue :col-type (or db-null text) :initarg :dialogue)
+   (parenthetical :col-type (or db-null text) :initarg :parenthetical)
+   (timestamp :col-type timestamp :col-default (:now) :initarg :timestamp))
+  (:keys id))
+(defun add-dialogue (scene-id actor-id dialogue parenthetical)
+  (make-dao 'scene-dialogue
+            :scene-id scene-id
+            :actor-id actor-id
+            :dialogue (or dialogue :null)
+            :parenthetical (or parenthetical :null)))
 
-(defun add-dialogue (scene-id actor dialogue parenthetical &key (timestamp (get-universal-time)))
-  (ensure-doc (get-uuid)
-              (mkdoc "type" "scene-entry"
-                     "entry_type" "dialogue"
-                     "scene_id" scene-id
-                     "actor" actor
-                     "dialogue" dialogue
-                     "parenthetical" parenthetical
-                     "timestamp" timestamp)))
+(defdao scene-upvote ()
+  ((id :col-type serial :reader id)
+   (scene-id :col-type bigint :initarg :scene-id)
+   (account-id :col-type bigint :initarg :account-id))
+  (:keys id))
 
-(defun scene-id (scene)
-  (doc-val scene "_id"))
-
-(defun find-scene-rating-by-account-name (scene-id account-name)
-  (view-query-value "scene" "rating_by_account_name" (list scene-id account-name)))
-
-(defun scene-upvote (scene-id account-name)
-  (unless (find-scene-rating-by-account-name scene-id account-name)
-    (ensure-doc (get-uuid)
-                (mkdoc "type" "scene-rating"
-                       "scene_id" scene-id
-                       "account_name" account-name))
+(defun scene-upvote (scene-id account-email)
+  (with-transaction ()
+    (unless (account-voted-p scene-id account-email)
+      (make-dao 'scene-upvote))
     t))
 
-(defun scene-rating (scene-id)
-  (view-query-value "scene" "rating_by_scene_id" scene-id))
+(defun scene-id (scene)
+  (id scene))
 
-(defun find-scenes-by-account-name (account-name)
-  (view-query-value "scene" "by_account_name" account-name nil))
+(defun account-voted-p (scene-id account-email)
+  (query (:select t :from (:as 'scene-upvote 'uv)
+                  :inner-join (:as 'account 'acc)
+                  :on (:= 'acc.id 'uv.account-id)
+                  :where (:and (:= 'uv.scene-id scene-id)
+                               (:= 'acc.email (string-downcase account-email))))
+         :single))
+
+(defun scene-rating (scene-id)
+  (query (:select (:count :*) :from 'scene-upvote
+                  :where (:= 'scene-id scene-id))))
+
+(defun find-scenes-by-account-email (account-email)
+  (when-let (account-id (query (:select 'id :from 'account
+                                        :where (:= 'email (string-downcase account-email)))
+                               :single))
+    (select-dao 'scene (:= 'account-id account-id))))
 
 (defun find-scene-with-entries (scene-id)
-  (when-let ((results (doc-val (query-view *db* "scene" "full_by_id"
-                                           :startkey (list scene-id)
-                                           :endkey (list scene-id 2))
-                               "rows")))
-    (mapcar (rcurry #'doc-val "value") results)))
+  (query (:select :* :from (:as 'scene 's)
+                  :left-join (:as 'dialogue 'd)
+                  :on (:= 'd.scene-id 's.id)
+                  :left-join (:as 'action 'a)
+                  :on (:= 'a.scene-id 's.id)
+                  :where (:= 's.id scene-id))
+         :alists))
 
 (defun find-scene-entries (scene-id)
-  (view-query-value "scene" "entries_by_scene_id" scene-id nil))
+  (find-scene-with-entries scene-id))
