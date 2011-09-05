314e75ed6d8ebd47fe35bca7d4900394c7af0bea
Kat Marchán <kzm@sykosomatic.org>
2011-09-04 22:04:57 -0400

    Named entities using modifiers.

--- src/entity.lisp
+++ src/entity.lisp
@@ -1,10 +1,12 @@
 (cl:defpackage #:sykosomatic.entity
-  (:use :cl :postmodern :sykosomatic.db))
+  (:use :cl :postmodern :sykosomatic.db)
+  (:export :list-modifiers :add-modifier :create-entity
+           :entity-name :find-entity-by-name))
 (cl:in-package #:sykosomatic.entity)
 
 (defdao entity ()
   ((id :col-type serial :reader id)
-   (comment :col-type text))
+   (comment :col-type (or db-null text)))
   (:keys id))
 
 (defdao modifier ()
@@ -37,3 +39,37 @@
               :timestamp-value (or timestamp-value :null)
               :description (or description :null)
               :precedence (or precedence 0))))
+
+;; TODO - It might be useful to be able to quickly identify entities by some kind of name.  Perhaps
+;;        we could just use a modifier, and have a modifier module that even gives multiple
+;;        namespaces. :)
+(defun create-entity (&key comment)
+  (id (make-dao 'entity :comment (or comment :null))))
+
+(defun entity-name (entity)
+  (query (:select 'text-value :from 'modifier
+                  :where (:and (:= 'entity-id (entity-id entity))
+                               (:= 'type "entity-name")))
+         :single))
+
+(defun (setf entity-name) (new-value entity)
+  (with-transaction ()
+    (if (entity-name entity)
+        (query (:update 'modifier
+                        :set 'text-value new-value
+                        :where (:and (:= 'entity-id (entity-id entity))
+                                     (:= 'type "entity-name"))))
+        (progn
+          (assert (not (query (:select t :from 'modifier
+                                       :where (:and (:= 'type "entity-name")
+                                                    (:= 'text-value new-value)))
+                              :single))
+                  () "~S is not a globally unique entity name." new-value)
+          (add-modifier entity "entity-name" :text-value new-value :description
+                        "Unique human-usable identifier for entity.")))))
+
+(defun find-entity-by-name (name)
+  (query (:select 'entity-id :from 'modifier
+                  :where (:and (:= 'type "entity-name")
+                               (:= 'text-value name)))
+         :single))
