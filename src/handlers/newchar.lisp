306a80ea303978b0e4fcfc70d2ad6206430113a1
Kat Marchán <kzm@sykosomatic.org>
2011-09-13 23:19:35 -0400

    Grabbing the newchar post parameters lisp-side.

--- src/handlers/newchar.lisp
+++ src/handlers/newchar.lisp
@@ -9,24 +9,40 @@
   (:export :newchar))
 (cl:in-package #:sykosomatic.handler.newchar)
 
-(define-easy-handler (newchar :uri "/newchar") ()
-  #+nil((careers :parameter-type 'array)
-        (career-times :parameter-type 'array)
-        (bodyparts :parameter-type 'array)
-        (bodypart-adjs :parameter-type 'array))
-  #+nil(ensure-logged-in)
-  (with-form-errors
-    (templ:render-template "newchar"
-                           :pronouns (cc-select-options "pronoun")
-                           :origins (cc-select-options "origin")
-                           :parents (cc-select-options "parents")
-                           :siblings (cc-select-options "siblings")
-                           :situations (cc-select-options "situation")
-                           :careers (cc-select-options "career")
-                           :friends (cc-select-options "friends")
-                           :so (cc-select-options "significant-other")
-                           :location-opts (cc-select-options "location")
-                           :features (cc-features))))
+(define-easy-handler (newchar :uri "/newchar") (pronoun
+                                                first-name
+                                                nickname
+                                                last-name
+                                                origin
+                                                parents
+                                                siblings
+                                                situation
+                                                friends
+                                                so
+                                                (careers :parameter-type 'array)
+                                                (career-times :parameter-type 'array)
+                                                (features :parameter-type 'array)
+                                                (feature-adjs :parameter-type 'array)
+                                                where)
+  (ensure-logged-in)
+  (case (request-method*)
+    (:get
+     (with-form-errors
+       (templ:render-template "newchar"
+                              :pronouns (cc-select-options "pronoun")
+                              :origins (cc-select-options "origin")
+                              :parents (cc-select-options "parents")
+                              :siblings (cc-select-options "siblings")
+                              :situations (cc-select-options "situation")
+                              :careers (cc-select-options "career")
+                              :friends (cc-select-options "friends")
+                              :so (cc-select-options "significant-other")
+                              :location-opts (cc-select-options "location")
+                              :features (cc-features))))
+    (:post
+     (print (list pronoun first-name nickname last-name origin parents siblings situation
+                  friends so careers career-times features feature-adjs where))
+     (redirect "/newchar"))))
 
 (define-easy-handler (newchar-feature-adjs :uri "/newchar/feature-adjs") (feature-name)
   (when-let (opts (cc-adjectives feature-name))
