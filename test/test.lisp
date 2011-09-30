a216902477ba55c452d6a1fcc4771afe4434adad
Kat Marchán <kzm@sykosomatic.org>
2011-09-30 15:06:06 -0400

    def-test-package for maximum convenience.

--- test/test.lisp
+++ test/test.lisp
@@ -1,6 +1,17 @@
-(cl:defpackage #:sykosomatic.test
-  (:use :cl :alexandria :eos)
-  (:export :sykosomatic))
-(cl:in-package #:sykosomatic.test)
+(util:def-file-package #:sykosomatic.test
+  (:use :eos)
+  (:nicknames :test)
+  (:export :sykosomatic :def-test-package))
 
 (def-suite sykosomatic)
+
+(defmacro def-test-package (suite-name &body defpackage-iargs)
+  (let ((package-name (concatenate 'string
+                                   (string '#:sykosomatic.test.)
+                                   (string suite-name))))
+    `(progn
+      (util:def-file-package ,package-name
+        (:use :eos :sykosomatic.test)
+        ,@defpackage-iargs)
+      (def-suite ,suite-name :in sykosomatic)
+      (in-suite ,suite-name))))
