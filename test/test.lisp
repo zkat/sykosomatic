da229916c699825a7a0c4a3b58a248fb006cfbdf
Kat Marchán <kzm@sykosomatic.org>
2011-09-30 20:35:23 -0400

    Getting pxref the info it needs.

--- test/test.lisp
+++ test/test.lisp
@@ -9,9 +9,13 @@
   (let ((package-name (concatenate 'string
                                    (string '#:sykosomatic.test.)
                                    (string suite-name))))
-    `(progn
-      (util:def-file-package ,package-name
-        (:use :eos :sykosomatic.test)
-        ,@defpackage-iargs)
-      (def-suite ,suite-name :in sykosomatic)
-      (in-suite ,suite-name))))
+    `(prog1
+         ;; Gotta return the package to make pxref happy.
+         (util:def-file-package ,package-name
+           (:use :eos :sykosomatic.test)
+           ,@defpackage-iargs)
+       (def-suite ,suite-name :in sykosomatic)
+       (in-suite ,suite-name))))
+(when (find-package :pxref)
+  (push 'def-test-package
+        (symbol-value (intern "*HANDLE-PACKAGE-FORMS*" :pxref))))
