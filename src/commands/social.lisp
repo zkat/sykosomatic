8c7fdca1a577a113f92d5cd4b17619a3a3f5eb65
Josh Marchán <sykopomp@sykosomatic.org>
2011-10-03 01:28:46 -0400

    Initial implementation of 'social' command.

--- src/commands/social.lisp
+++ src/commands/social.lisp
@@ -1,8 +1,41 @@
 (util:def-file-package #:sykosomatic.command.social
   (:use :sykosomatic.command
+        :sykosomatic.vocabulary
         :sykosomatic.game-objects.nameable))
 
 (defcommand social ()
-  (tell-local *actor* "~A ~A."
-              (full-name *actor*)
-              *verb*))
+  (tell-local *actor* (with-output-to-string (s)
+                        (princ (full-name *actor*) s)
+                        (when (eql 0 *adverb-position*)
+                          (format s " ~A " *adverb*))
+                        (format s " ~A" (verb-third-person *verb*))
+                        (when (eql 1 *adverb-position*)
+                          (format s " ~A" *adverb*))
+                        (cond (*preposition*
+                               (when *direct-objects* (princ " " s))
+                               (format s *english-list-format-string* (mapcar #'full-name
+                                                                              *direct-objects*))
+                               (when (eql 2 *adverb-position*)
+                                 (format s " ~A" *adverb*))
+                               (format s " ~A" *preposition*)
+                               (when *indirect-objects* (princ " " s))
+                               (format s *english-list-format-string* (mapcar #'full-name
+                                                                              *indirect-objects*)))
+                              (t
+                               (when *indirect-objects* (princ " " s))
+                               (format s *english-list-format-string* (mapcar #'full-name
+                                                                              *indirect-objects*))
+                               (when *direct-objects* (princ " " s))
+                               (format s *english-list-format-string* (mapcar #'full-name
+                                                                              *direct-objects*))
+                               (when (eql 2 *adverb-position*)
+                                 (format s " ~A" *adverb*))))
+                        (when (eql 3 *adverb-position*)
+                          (format s " ~A" *adverb*))
+                        (princ "." s))))
+
+;;; Testing
+#+nil
+(add-verb-command "smiles" 'social)
+#+nil
+(add-verb-command "punches" 'social)
