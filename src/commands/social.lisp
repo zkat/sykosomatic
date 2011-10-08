ac1a7fe0790d602efbe3ce7b0a6a998eed864ce2
Josh Marchán <sykopomp@sykosomatic.org>
2011-10-07 23:44:40 -0400

    New way to handle telling all 'local' entities.

--- src/commands/social.lisp
+++ src/commands/social.lisp
@@ -4,30 +4,31 @@
         :sykosomatic.components.describable))
 
 (defcommand social ()
-  (tell-local *actor* (with-output-to-string (s)
-                        (princ (short-description *actor* *actor*) s)
-                        (when (eql 0 *adverb-position*)
-                          (format s " ~A " *adverb*))
-                        (format s " ~A" (verb-third-person *verb*))
-                        (when (eql 1 *adverb-position*)
-                          (format s " ~A" *adverb*))
-                        (when (null *preposition*)
-                          (when *indirect-objects* (princ " " s))
-                          (format s *english-list-format-string* (mapcar #'short-description
-                                                                         *indirect-objects*)))
-                        (when *direct-objects* (princ " " s))
-                        (format s *english-list-format-string* (mapcar #'short-description
-                                                                       *direct-objects*))
-                        (when (eql 2 *adverb-position*)
-                          (format s " ~A" *adverb*))
-                        (when *preposition*
-                          (format s " ~A" *preposition*)
-                          (when *indirect-objects* (princ " " s))
-                          (format s *english-list-format-string* (mapcar #'short-description
-                                                                         *indirect-objects*)))
-                        (when (eql 3 *adverb-position*)
-                          (format s " ~A" *adverb*))
-                        (princ "." s))))
+  (do-local-entities (e *actor*)
+    (tell e (with-output-to-string (s)
+              (princ (short-description *actor* *actor*) s)
+              (when (eql 0 *adverb-position*)
+                (format s " ~A " *adverb*))
+              (format s " ~A" (verb-third-person *verb*))
+              (when (eql 1 *adverb-position*)
+                (format s " ~A" *adverb*))
+              (when (null *preposition*)
+                (when *indirect-objects* (princ " " s))
+                (format s *english-list-format-string* (mapcar #'short-description
+                                                               *indirect-objects*)))
+              (when *direct-objects* (princ " " s))
+              (format s *english-list-format-string* (mapcar #'short-description
+                                                             *direct-objects*))
+              (when (eql 2 *adverb-position*)
+                (format s " ~A" *adverb*))
+              (when *preposition*
+                (format s " ~A" *preposition*)
+                (when *indirect-objects* (princ " " s))
+                (format s *english-list-format-string* (mapcar #'short-description
+                                                               *indirect-objects*)))
+              (when (eql 3 *adverb-position*)
+                (format s " ~A" *adverb*))
+              (princ "." s)))))
 
 ;;; Testing
 #+nil
