e4621c8810c019d86b45ef55589b74b01348c073
Kat Marchán <kzm@sykosomatic.org>
2011-10-12 14:09:54 -0400

    Nicer field rendering API; template fixups.

--- src/template.lisp
+++ src/template.lisp
@@ -1,8 +1,9 @@
 (util:def-file-package :sykosomatic.template
   (:use :html-template
+        :sykosomatic.util.form
         :sykosomatic.config)
   (:export :render-template
-           :text-input-field
+           :text-field
            :select-field
            :field-optgroup))
 
@@ -18,27 +19,51 @@
                                variables))))
 
 ;; Utilities
-(defun text-input-field (name label &key id max-length type value error)
-  (list #p"text-input-field.html"
+(defun labelify (dashed-string)
+  (format nil "~:(~A~)" (substitute #\space #\- dashed-string)))
+
+(defun text-field (form field-name &key
+                   (name (string-downcase field-name))
+                   (label (labelify (string field-name)))
+                   (id (concatenate 'string (string-downcase field-name) "-field"))
+                   max-length
+                   class
+                   (type "text")
+                   (template #p"fields/text.html")
+                   (value (field-raw-value form field-name))
+                   (error (field-error form field-name)))
+  (list template
         :field-id id :field-name name :field-label label
-        :field-value value :field-error error :field-type type
-        :field-max-length max-length))
+        :field-value value
+        :field-error error
+        :field-type type
+        :field-max-length max-length
+        :field-class class))
 
 (defun field-optgroup (label &optional options)
   `(:field-optgroup-label ,label :field-optgroup-options ,options))
 
-(defun select-field (name label &key optgroups default-text id class error value)
-  `(#p "select-input-field.html"
-       :field-name ,name
-       :field-label ,label
-       :field-id ,id
-       :field-class ,class
-       :field-optgroups ,(loop for optgroup in optgroups
+(defun select-field (form field-name &key
+                     (name (string-downcase field-name))
+                     (label (englishify (string field-name)))
+                     (id (concatenate 'string (string-downcase field-name) "-field"))
+                     class
+                     (template #p"fields/select.html")
+                     (value (field-raw-value form field-name))
+                     (error (field-error form field-name))
+                     optgroups
+                     default-text)
+  (list template
+        :field-name name
+        :field-label label
+        :field-id id
+        :field-class class
+        :field-optgroups (loop for optgroup in optgroups
                             collect (field-optgroup (getf optgroup :field-optgroup-label)
                                                     (maybe-mark-selected value (getf optgroup :field-optgroup-options))))
-       :field-default-text ,default-text
-       :field-error ,error
-       :field-value ,value))
+        :field-default-text default-text
+        :field-error error
+        :field-value value))
 
 (defun maybe-mark-selected (target-value options)
   (loop for opt in options
